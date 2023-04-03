63c2f80a72eb796b26864afb6f28bb0706a24370
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-04-03 12:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const evaluatedText = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+evaluatedText.addEventListener("input", () => {
+  const text = evaluatedText.value.trim();
+  const count = text ? text.split(/\s+/).length : 0;
+  wordCount.textContent = count;
+});
+
